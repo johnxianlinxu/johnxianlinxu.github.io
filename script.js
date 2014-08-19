e3bff51747c20b136879431c82e4f02c22ae6ac1
John Xu <johnxianlinxu@yahoo.ca>
2014-08-19 00:55:59 -0400

    Added navbar and jQuery test page

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    for (var i = 0; i  1000; i++) {
+        $('body').append("<div></div>");
+    }
+    $(document).on("mouseenter", "div",
+        function() {
+            $(this).fadeTo(500, 0);
+        });
+});
